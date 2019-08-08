5a84782a05a82f715fdb57392abc5f8401300e8b
narintorn <53886393+narintorn@users.noreply.github.com>
2019-08-08 10:39:10 +0700

    Add files via upload

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v4.7.5',
+  'full_app_version' => 'v4.7.5 - build 4137-g55ee90b25',
+  'build_version' => '4137',
+  'prerelease_version' => '',
+  'hash_version' => 'g55ee90b25',
+  'full_hash' => 'v4.7.5-18-g55ee90b25',
+  'branch' => 'master',
+);
